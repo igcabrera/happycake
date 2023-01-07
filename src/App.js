980bb8c9438c13e89ba9cecb3a61069f77ddce58
igcabrera <icabrera.illanes@gmail.com>
2023-01-06 21:16:46 -0300

    cambio ruta happycakea

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/happycake' element={<Home />} />
         <Route path='contacto' element={<Contacto />} />
       </Routes>
     </BrowserRouter>
